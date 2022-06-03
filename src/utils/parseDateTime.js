bb3f5eff65d623a93578c76aca17252309113d64
Vitaliy Zavgorodniy <vince.kkr@gmail.com>
2022-06-03 06:30:01 +0300

    fixed timeformatter util

--- src/utils/parseDateTime.js
+++
@@ -1,6 +0,0 @@
-import { DateTime } from 'luxon';
-
-const TIME_ZONE = { zone: 'Europe/Kiev' };
-
-export const parseDateTime = (date) =>
-  DateTime.fromJSDate(date, TIME_ZONE).toString();
--- src/utils/timeFormatToString.js
+++ src/utils/timeFormatToString.js
@@ -2,5 +2,10 @@ import { DateTime } from 'luxon';
 
 const TIME_ZONE = { zone: 'Europe/Kiev' };
 
-export const timeFormatToString = (date) =>
-  DateTime.fromJSDate(date, TIME_ZONE).toString();
+export const timeFormatToString = (date) => {
+  const parsedDate = DateTime.fromJSDate(date, TIME_ZONE).toString();
+
+  if (parsedDate === 'Invalid DateTime') return date;
+
+  return parsedDate;
+};
