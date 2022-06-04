c4643f72109a615a3eda4efa0a1c633b23797704
sasha87972 <82048515+sasha87972@users.noreply.github.com>
2022-06-04 19:28:27 +0300

    modal wrapper and review form added

---
+++ src/components/UI-kit/inputs/Modal.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { createPortal } from "react-dom";
+import styled from 'styled-components';
+
+
+const Modal = ({ children, onClose }) => {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    document.body.style.overflow = 'hidden';
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
+  const handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
+    }
+  };
+
+  return createPortal(
+    <Overlay onClick={handleBackdropClick}>
+      {children}
+    </Overlay>
+  );
+};
+
+
+const Overlay = styled.div`
+z-index: 1200;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.7);
+`
+
+export default Modal;
