35455f419a98cfa449c90a777b5b923779cd8371
MaryanaDey <81640002+MaryanaDey@users.noreply.github.com>
2022-06-05 16:42:23 +0200

    init button

--- src/components/UI-kit/buttons/Button/Button.js
+++ src/components/UI-kit/buttons/Button/Button.js
@@ -1,11 +1,34 @@
+import styled from 'styled-components';
+import { breakpoints } from 'constants/breakpoints';
+
 const Button = ({ type, text, isActive, onClick = null }) => {
     return (
-        <button onClick={onClick}>
+        <Buttontext onClick={onClick}>
             {text}
-        </button>
+        </Buttontext>
     );
 
 };
+
+const Buttontext = styled.button`
+width: 280px;
+height: 60px;
+background: #FF6B08;
+color: #fff;
+ &:hover {
+    cursor: pointer;
+    background-color: #b2b2b2;
+  }
+    @media ${breakpoints.tablet} {
+    width: 320px;
+    height: 60px;
+  }
+    @media ${breakpoints.desktop} {
+    width: 320px;
+    height: 60px;
+  }
+
+`;
 
 export default Button;
 
