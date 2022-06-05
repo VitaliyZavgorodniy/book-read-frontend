7d3bb8ee68a5f401445613eb69258c46fb802d08
MaryanaDey <81640002+MaryanaDey@users.noreply.github.com>
2022-06-05 11:08:37 +0200

    init feature/autorization

--- src/pages/LoginPage/LoginPages.jsx
+++ src/pages/LoginPage/LoginPages.jsx
@@ -1,3 +1,29 @@
-const LoginPage = () => <div>Login Page</div>;
+import styled from 'styled-components';
+import LoginForm from 'components/LoginForm';
+
+const LoginPage = () => {
+  return (
+    <Wrapper>
+      <LoginBlock>
+        <LoginForm />
+      </LoginBlock>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const LoginBlock = styled.div`
+  padding: 90px 75px;
+  background-color: ${(p) => p.theme.colors.bgAlpha};
+  background-image: linear-gradient(
+      to right,
+      ${(p) => p.theme.colors.bgAlpha},
+      ${(p) => p.theme.colors.bgAlpha}
+    ),
+    url(${(p) => p.theme.backgrounds.register});
+`;
 
 export default LoginPage;
