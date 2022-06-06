424b99b252174bf874410d755e171c80cf521c94
Oksana Stets <oksanastets73@gmail.com>
2022-06-07 00:40:30 +0300

    add adaptive

--- src/components/StatisticTable/StatisticTable.jsx
+++ src/components/StatisticTable/StatisticTable.jsx
@@ -1,13 +1,33 @@
 import styled from 'styled-components';
+import Media from 'react-media';
+
+import { breakpoints } from 'constants/breakpoints';
 
 const StatisticTable = ({ items }) => (
   <Table>
     <thead>
-      <StatRow>
-        <TableHeaderDecor></TableHeaderDecor>
-        <TableHeader>Statistics</TableHeader>
-        <TableHeaderDecor></TableHeaderDecor>
-      </StatRow>
+      <Media
+        queries={{
+          medium: '(min-width: 768px) and (max-width: 1279px)',
+        }}
+      >
+        {(matches) =>
+          matches.medium ? (
+            <StatRow>
+              <TableHeader>Statistics</TableHeader>
+              <TableHeaderDecor></TableHeaderDecor>
+              <TableHeaderDecor></TableHeaderDecor>
+            </StatRow>
+          ) : (
+            <StatRow>
+              <TableHeaderDecor></TableHeaderDecor>
+              <TableHeader>Statistics</TableHeader>
+              <TableHeaderDecor></TableHeaderDecor>
+            </StatRow>
+          )
+        }
+      </Media>
+     
     </thead>
 
     <tbody>
@@ -32,7 +52,6 @@ const Table = styled.table`
   margin-left: auto;
   margin-right: auto;
   color: ${(p) => p.theme.colors.primary};
-  /* border: 1px solid red; */
   border-collapse: collapse;
 `;
 
@@ -44,8 +63,12 @@ const TableHeader = styled.th`
   font-size: 12px;
   line-height: 1.25;
   text-align: center;
-  /* border: 1px solid red; */
   border-collapse: collapse;
+
+  @media ${breakpoints.tablet} {
+    padding: 0;
+     text-align: start;
+  }
 `;
 const TableHeaderDecor = styled(TableHeader)`
   padding: 0;
@@ -60,7 +83,7 @@ const TableHeaderDecor = styled(TableHeader)`
   }
 `;
 const StatRow = styled.tr`
- margin-bottom: 4px;
+  margin-bottom: 4px;
 `;
 const StatRowBody = styled(StatRow)`
   font-size: 14px;
@@ -72,8 +95,8 @@ const StatColumnLight = styled(StatColumn)`
   color: ${(p) => p.theme.colors.tertiary};
 `;
 const StatColumnPages = styled.td`
-   text-align: end; 
-`
+  text-align: end;
+`;
 const Pages = styled.span`
   color: ${(p) => p.theme.colors.tertiary};
 `;
