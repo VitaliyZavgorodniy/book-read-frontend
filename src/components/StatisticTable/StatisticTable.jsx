1f609f8bc9c5153cf1fe479f7f730f3ffb17cd0f
Oksana Stets <oksanastets73@gmail.com>
2022-06-06 23:39:42 +0300

    add styles

--- src/components/StatisticTable/StatisticTable.jsx
+++ src/components/StatisticTable/StatisticTable.jsx
@@ -3,23 +3,23 @@ import styled from 'styled-components';
 const StatisticTable = ({ items }) => (
   <Table>
     <thead>
-      <tr>
+      <StatRow>
         <TableHeaderDecor></TableHeaderDecor>
         <TableHeader>Statistics</TableHeader>
         <TableHeaderDecor></TableHeaderDecor>
-      </tr>
+      </StatRow>
     </thead>
 
     <tbody>
       {items.map((item) => (
-        <tr key={item.id}>
-          <td>{item.date}</td>
-          <td>{item.time}</td>
-          <td>
+        <StatRowBody key={item.id}>
+          <StatColumn>{item.date}</StatColumn>
+          <StatColumnLight>{item.time}</StatColumnLight>
+          <StatColumnPages>
             {item.pages}
-            <span>pages</span>
-          </td>
-        </tr>
+            <Pages>&nbsp;pages</Pages>
+          </StatColumnPages>
+        </StatRowBody>
       ))}
     </tbody>
   </Table>
@@ -32,29 +32,48 @@ const Table = styled.table`
   margin-left: auto;
   margin-right: auto;
   color: ${(p) => p.theme.colors.primary};
-  border: 1px solid red;
+  /* border: 1px solid red; */
   border-collapse: collapse;
 `;
 
 const TableHeader = styled.th`
- position: relative;
- /* padding: 0 13px 0 12px; */
+  position: relative;
+  padding: 0 13px 0 12px;
   text-transform: uppercase;
   font-weight: 700;
   font-size: 12px;
   line-height: 1.25;
   text-align: center;
-  border: 1px solid red;
+  /* border: 1px solid red; */
   border-collapse: collapse;
 `;
 const TableHeaderDecor = styled(TableHeader)`
-&:after{
-    content: "";
+  padding: 0;
+  &:after {
+    content: '';
     display: block;
     width: 100%;
     height: 1px;
     background-color: ${(p) => p.theme.colors.lineStat};
     position: absolute;
     top: 50%;
-}
+  }
+`;
+const StatRow = styled.tr`
+ margin-bottom: 4px;
+`;
+const StatRowBody = styled(StatRow)`
+  font-size: 14px;
+  line-height: 1.22;
+`;
+const StatColumn = styled.td``;
+const StatColumnLight = styled(StatColumn)`
+  text-align: center;
+  color: ${(p) => p.theme.colors.tertiary};
+`;
+const StatColumnPages = styled.td`
+   text-align: end; 
 `
+const Pages = styled.span`
+  color: ${(p) => p.theme.colors.tertiary};
+`;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,10 +6,10 @@ import StatisticTable from 'components/StatisticTable';
 // Экран создания книг
 const data = [
   { "id": "id-1", "date": "10.10.2019", "time": "08:10:23", "pages": 22 },
-  { "id": "id-2", "date": "11.10.2019", "time": "08:10:23", "pages": 4 },
-  { "id": "id-3", "date": "12.10.2019", "time": "08:10:23", "pages": 17 },
-  { "id": "id-4", "date": "13.10.2019", "time": "08:10:23", "pages": 47 },
-  { "id": "id-5", "date": "14.10.2019", "time": "08:10:23", "pages": 10 }
+  { "id": "id-2", "date": "11.10.2019", "time": "23:50:15", "pages": 164 },
+  { "id": "id-3", "date": "12.10.2019", "time": "13:10:05", "pages": 132 },
+  { "id": "id-4", "date": "13.10.2019", "time": "11:10:07", "pages": 137 },
+  { "id": "id-5", "date": "14.10.2019", "time": "10:10:11", "pages": 10 }
 ];
 const HomePage = ({ library, items }) => {
   return (
