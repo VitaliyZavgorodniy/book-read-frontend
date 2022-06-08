5e150ca5d0b944081b771872602b429058898370
Vitaliy Zavgorodniy <vince.kkr@gmail.com>
2022-06-08 18:36:42 +0300

    added reating component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react';
 import styled from 'styled-components';
 
 import InfoBlockIntro from './InfoBlockIntro';
 
-import StarRating from 'components/UI-kit/inputs/StarRating';
 // Экран создания книг
 const HomePage = () => {
   return (
